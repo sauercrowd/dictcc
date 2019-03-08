3348a98e59b6aa5d14fea1c24aa3d2deaac98825
sauercrowd <sauercrowd@posteo.no>
2019-03-08 21:46:03 +0000

    init

---
+++ langs.go
@@ -0,0 +1,46 @@
+package main
+
+var langs = map[string]string{
+	"de": "la",
+	"de": "nl",
+	"de": "no",
+	"de": "pl",
+	"de": "pt",
+	"de": "ro",
+	"de": "ru",
+	"de": "sk",
+	"de": "sq",
+	"de": "sr",
+	"de": "sv",
+	"de": "tr",
+	"de": "en",
+	"en": "bg",
+	"en": "bs",
+	"en": "cs",
+	"en": "da",
+	"en": "el",
+	"en": "eo",
+	"en": "es",
+	"en": "fi",
+	"en": "fr",
+	"en": "hr",
+	"en": "hu",
+	"en": "is",
+	"en": "it",
+	"en": "la",
+	"en": "nl",
+	"en": "no",
+	"en": "pl",
+	"en": "pt",
+	"en": "ro",
+	"en": "ru",
+	"en": "sk",
+	"en": "sq",
+	"en": "sr",
+	"en": "sv",
+	"en": "tr",
+}
+
+func ValidLangPair(sourceLang, targetLang) {
+
+}
---
+++ main.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/olekukonko/tablewriter"
+	"github.com/robertkrimen/otto/ast"
+	"github.com/robertkrimen/otto/parser"
+	"golang.org/x/net/html"
+)
+
+type Translation struct {
+	textSourceLang string
+	textTargetLang string
+}
+
+func processJS(js string) []Translation {
+	program, err := parser.ParseFile(nil, "", js, 0)
+	if err != nil {
+		panic(err)
+	}
+
+	var sourceElements []string
+	var targetElements []string
+	for _, x := range program.Body {
+		vstmnt, ok := x.(*ast.VariableStatement)
+		if !ok {
+			continue
+		}
+
+		for _, expr := range vstmnt.List {
+			vexpr, ok := expr.(*ast.VariableExpression)
+			if !ok {
+				continue
+			}
+			//source language
+			if vexpr.Name == "c1Arr" {
+				sourceElements = parseNewExpression(vexpr.Initializer)
+			}
+			//target language
+			if vexpr.Name == "c2Arr" {
+				targetElements = parseNewExpression(vexpr.Initializer)
+			}
+		}
+
+	}
+
+	if len(sourceElements) != len(targetElements) {
+		return nil
+	}
+	translations := make([]Translation, 0, len(sourceElements))
+	for i, sourceElement := range sourceElements {
+		translation := Translation{
+			textSourceLang: sourceElement,
+			textTargetLang: targetElements[i],
+		}
+		translations = append(translations, translation)
+	}
+	return translations
+}
+
+func extractJS(body io.Reader) string {
+	tokeniser := html.NewTokenizer(body)
+	triggered := false
+	for {
+		tt := tokeniser.Next()
+		if tt == html.ErrorToken {
+			break
+		}
+		if triggered && tt == html.TextToken {
+			text := string(tokeniser.Text())
+			if strings.Contains(text, "var c1Arr = new Array") {
+				return text
+			}
+		}
+		if tt == html.StartTagToken && tokeniser.Token().String() == `<script type="text/javascript">` {
+			triggered = true
+		}
+		if tt == html.EndTagToken && tokeniser.Token().String() == "</script>" {
+			triggered = false
+		}
+	}
+	return ""
+}
+
+func parseNewExpression(node ast.Expression) []string {
+	nexpr, ok := node.(*ast.NewExpression)
+	if !ok {
+		return nil
+	}
+	elements := make([]string, 0)
+	for _, arg := range nexpr.ArgumentList {
+		sliteral, ok := arg.(*ast.StringLiteral)
+		if !ok {
+			continue
+		}
+		elements = append(elements, sliteral.Value)
+	}
+	return elements
+}
+
+const allowedCombinations = map[string]string{}
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a search term")
+	}
+	search := strings.Join(os.Args[1:], " ")
+	u := "https://www.dict.cc/?s=" + url.QueryEscape(search)
+	resp, err := http.Get(u)
+	if err != nil {
+		panic(err)
+	}
+	js := extractJS(resp.Body)
+	resp.Body.Close()
+	translations := processJS(js)
+
+	table := tablewriter.NewWriter(os.Stdout)
+	table.SetHeader([]string{"Source", "Target"})
+
+	for _, translation := range translations {
+		table.Append([]string{translation.textSourceLang, translation.textTargetLang})
+	}
+	table.Render()
+}
